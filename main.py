a2f6b923505823415ba3180fafbac1ce128ede4d
alugocp <alugocp@aim.com>
2025-02-07 14:07:30 -0500

    Documentation

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+Generates puzzlebox dungeons like those seen in the Zelda series
+"""
+
 from typing import Generic, TypeVar, Self
 from functools import reduce
 from random import choice
@@ -6,11 +10,16 @@ from dataclasses import dataclass
 from math import inf
 
 
+# Generic type definitions for the Graph class
 N = TypeVar("N")
 E = TypeVar("E")
 
 
 class Graph(Generic[N, E]):
+    """
+    Generic implementation of a collection of nodes and edges
+    """
+
     nodes: list[N]
     edges: dict[str, E]
 
@@ -19,25 +28,39 @@ class Graph(Generic[N, E]):
         self.edges = {}
 
     def add_node(self, node: N):
+        """
+        Adds a node to the Graph
+        """
         self.nodes.append(node)
 
     def add_edge(self, n1: N, n2: N, label: E, directional=False):
+        """
+        Adds an edge to the Graph
+        """
         self.edges[self.get_edge_hash(n1, n2)] = label
         if not directional:
             self.edges[self.get_edge_hash(n2, n1)] = label
 
     def get_edge_hash(self, n1: N, n2: N) -> str:
+        """
+        Returns a unique value for the edge between the given nodes
+        """
         return str(hash(n1)) + ":" + str(hash(n2))
 
     def get_neighbors(self, node: N) -> list[N]:
-        h = hash(node)
+        """
+        Returns all nodes that the given node points to
+        """
         neighbors: list[N] = []
         for n in self.nodes:
-            if node != n and self.get_edge_hash(node, n) in self.edges.keys():
+            if node != n and self.get_edge_hash(node, n) in self.edges:
                 neighbors.append(n)
         return neighbors
 
     def random_walk(self) -> list[N]:
+        """
+        Returns a random path through the Graph
+        """
         walk: list[N] = []
         options = [self.nodes[0]]
         while len(options) > 0:
@@ -48,16 +71,14 @@ class Graph(Generic[N, E]):
         return walk
 
     def __str__(self) -> str:
-        result = (
-            "Nodes: " + ", ".join(list(map(lambda x: str(x), self.nodes))) + "\nEdges:"
-        )
+        result = "Nodes: " + ", ".join(list(map(str, self.nodes))) + "\nEdges:"
         edges = False
         for i1 in range(len(self.nodes) - 1):
             for i2 in range(i1 + 1, len(self.nodes)):
                 n1 = self.nodes[i1]
                 n2 = self.nodes[i2]
-                right = self.get_edge_hash(n1, n2) in self.edges.keys()
-                left = self.get_edge_hash(n2, n1) in self.edges.keys()
+                right = self.get_edge_hash(n1, n2) in self.edges
+                left = self.get_edge_hash(n2, n1) in self.edges
                 if right or left:
                     r = ">" if right else "-"
                     l = "<" if left else "-"
@@ -70,83 +91,114 @@ class Graph(Generic[N, E]):
 
 @dataclass
 class StateVar:
+    """
+    Represents a single component of State
+    """
+
     reversible: bool
     states: int
     value: int = 0
 
 
 class State:
-    vars: list[StateVar]
+    """
+    Represents some global state that a dungeon can be in
+    """
 
-    def __init__(self, vars: list[StateVar]):
-        self.vars = vars
+    state_vars: list[StateVar]
+
+    def __init__(self, state_vars: list[StateVar]):
+        self.state_vars = state_vars
 
     def __str__(self) -> str:
         return "".join(
-            [["F", "T"][x.value] if x.states == 2 else x.value for x in self.vars]
+            [["F", "T"][x.value] if x.states == 2 else x.value for x in self.state_vars]
         )
 
-    def all_are_max(self):
-        for var in self.vars:
+    def all_are_max(self) -> bool:
+        """
+        Returns True if all StateVars have their maximum value
+        """
+        for var in self.state_vars:
             if var.value < var.states - 1:
                 return False
         return True
 
     def increment(self):
-        i = len(self.vars) - 1
+        """
+        Increments this State by 1
+        """
+        i = len(self.state_vars) - 1
         while i >= 0:
-            if self.vars[i].value == self.vars[i].states - 1:
-                self.vars[i].value = 0
+            if self.state_vars[i].value == self.state_vars[i].states - 1:
+                self.state_vars[i].value = 0
                 i -= 1
             else:
-                self.vars[i].value += 1
+                self.state_vars[i].value += 1
                 break
 
     def __hash__(self) -> int:
-        value = 1 if self.vars[0].value == inf else self.vars[0].value
-        coeff = self.vars[0].states
-        for i in range(1, len(self.vars)):
-            value += coeff * (1 if self.vars[i].value == inf else self.vars[i].value)
-            coeff *= self.vars[i].states
+        value = 1 if self.state_vars[0].value == inf else self.state_vars[0].value
+        coeff = self.state_vars[0].states
+        for i in range(1, len(self.state_vars)):
+            value += coeff * (
+                1 if self.state_vars[i].value == inf else self.state_vars[i].value
+            )
+            coeff *= self.state_vars[i].states
         return value
 
     def __eq__(self, other: Self) -> bool:
-        for i in range(len(self.vars)):
-            if self.vars[i] != other.vars[i]:
+        for i in range(len(self.state_vars)):
+            if self.state_vars[i] != other.state_vars[i]:
                 return False
         return True
 
     def __sub__(self, other: Self) -> Self:
-        vars = deepcopy(self.vars)
-        for i in range(len(self.vars)):
-            vars[i].value = self.vars[i].value - other.vars[i].value
-        return StateDiff(vars)
+        state_vars = deepcopy(self.state_vars)
+        for i in range(len(self.state_vars)):
+            state_vars[i].value = self.state_vars[i].value - other.state_vars[i].value
+        return StateDiff(state_vars)
 
 
 class StateDiff(State):
+    """
+    Represents the difference between two State objects
+    """
 
-    def __init__(self, vars: list[StateVar]):
-        super().__init__(vars)
-        for var in self.vars:
+    def __init__(self, state_vars: list[StateVar]):
+        super().__init__(state_vars)
+        for var in self.state_vars:
             if var.value != 0 and var.reversible:
                 var.value = inf
 
     def __str__(self) -> str:
         mask = {inf: "*", 0: "."}
         return "".join(
-            [mask[x.value] if x.value in mask else str(x.value) for x in self.vars]
+            [
+                mask[x.value] if x.value in mask else str(x.value)
+                for x in self.state_vars
+            ]
         )
 
     def relevant(self) -> StateVar:
-        return next(filter(lambda x: x.value != 0, self.vars))
+        """
+        Returns the first StateVar with a non-zero value
+        """
+        return next(filter(lambda x: x.value != 0, self.state_vars))
 
     def __abs__(self) -> int:
         return reduce(
-            lambda acc, x: acc + abs(1 if x.value == inf else x.value), self.vars, 0
+            lambda acc, x: acc + abs(1 if x.value == inf else x.value),
+            self.state_vars,
+            0,
         )
 
 
 class Enclave:
+    """
+    Represents a collection of mutually accessible rooms in the dungeon
+    """
+
     mechanism: StateDiff | None
     turned_on: list[State] = []
 
@@ -163,17 +215,27 @@ class Enclave:
         return 0 if self.mechanism is None else hash(self.mechanism)
 
     def is_boss_enclave(self) -> bool:
+        """
+        Returns True if this is the last Enclave in the dungeon
+        """
         return self.mechanism is None
 
     def add_state(self, s: State):
+        """
+        Adds a State in which this Enclave will be accessible
+        """
         self.turned_on.append(s)
 
 
+# Convenient type definitions
 StateGraph = Graph[State, None]
 Dungeon = Graph[Enclave, None]
 
 
 def generate_state_graph(initial_state: State) -> StateGraph:
+    """
+    Generates a Graph of all possible States given some initial State
+    """
     g: StateGraph = Graph()
     curr = initial_state
     g.add_node(curr)
@@ -189,6 +251,9 @@ def generate_state_graph(initial_state: State) -> StateGraph:
 
 
 def get_enclaves(state_walk) -> Dungeon:
+    """
+    Returns a Graph of dungeon Enclaves without any edges yet
+    """
     g: Dungeon = Graph()
     for i in range(1, len(state_walk)):
         diff = state_walk[i] - state_walk[i - 1]
@@ -199,6 +264,9 @@ def get_enclaves(state_walk) -> Dungeon:
 
 
 def main(initial_state: State):
+    """
+    Generates and displays a new dungeon
+    """
     state_graph = generate_state_graph(initial_state)
     print("STATE GRAPH")
     print(state_graph)
@@ -206,7 +274,7 @@ def main(initial_state: State):
 
     state_walk = state_graph.random_walk()
     print("STATE WALK")
-    print(", ".join(map(lambda x: str(x), state_walk)))
+    print(", ".join(map(str, state_walk)))
     print("")
 
     enclaves = get_enclaves(state_walk)
