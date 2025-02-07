8c59a636c457e360a455231a392d4250da8e7fa1
alugocp <alugocp@aim.com>
2025-02-07 12:06:51 -0500

    Implemented enclaves

--- main.py
+++ main.py
@@ -2,60 +2,8 @@ from typing import Generic, TypeVar, Self
 from functools import reduce
 from random import choice
 from copy import deepcopy
-
-
-class StateVar:
-    reversible: bool
-    states: int
-    value: int
-
-    def __init__(self, reversible: bool, states: int):
-        self.reversible = reversible
-        self.states = states
-        self.value = 0
-
-    def __str__(self) -> str:
-        if self.states == 2:
-            return ["F", "T"][self.value]
-        return str(self.value)
-
-
-class State:
-    vars: list[StateVar]
-
-    def __init__(self, vars: list[StateVar]):
-        self.vars = vars
-
-    def __str__(self) -> str:
-        return "".join(list(map(lambda x: str(x), self.vars)))
-
-    def all_are_max(self):
-        for var in self.vars:
-            if var.value < var.states - 1:
-                return False
-        return True
-
-    def increment(self):
-        i = len(self.vars) - 1
-        while i >= 0:
-            if self.vars[i].value == self.vars[i].states - 1:
-                self.vars[i].value = 0
-                i -= 1
-            else:
-                self.vars[i].value += 1
-                break
-
-    def __sub__(self, other: Self) -> Self:
-        result = deepcopy(self)
-        for i in range(len(self.vars)):
-            result.vars[i].value = self.vars[i].value - other.vars[i].value
-        return result
-
-    def __abs__(self) -> int:
-        return reduce(lambda acc, x: acc + abs(x.value), self.vars, 0)
-
-    def relevant(self) -> StateVar:
-        return next(filter(lambda x: x.value != 0, self.vars))
+from dataclasses import dataclass
+from math import inf
 
 
 N = TypeVar("N")
@@ -120,8 +68,113 @@ class Graph(Generic[N, E]):
         return result
 
 
-def generate_state_graph(initial_state: State) -> Graph[State, None]:
-    g: Graph[State, None] = Graph()
+@dataclass
+class StateVar:
+    reversible: bool
+    states: int
+    value: int = 0
+
+
+class State:
+    vars: list[StateVar]
+
+    def __init__(self, vars: list[StateVar]):
+        self.vars = vars
+
+    def __str__(self) -> str:
+        return "".join(
+            [["F", "T"][x.value] if x.states == 2 else x.value for x in self.vars]
+        )
+
+    def all_are_max(self):
+        for var in self.vars:
+            if var.value < var.states - 1:
+                return False
+        return True
+
+    def increment(self):
+        i = len(self.vars) - 1
+        while i >= 0:
+            if self.vars[i].value == self.vars[i].states - 1:
+                self.vars[i].value = 0
+                i -= 1
+            else:
+                self.vars[i].value += 1
+                break
+
+    def __hash__(self) -> int:
+        value = 1 if self.vars[0].value == inf else self.vars[0].value
+        coeff = self.vars[0].states
+        for i in range(1, len(self.vars)):
+            value += coeff * (1 if self.vars[i].value == inf else self.vars[i].value)
+            coeff *= self.vars[i].states
+        return value
+
+    def __eq__(self, other: Self) -> bool:
+        for i in range(len(self.vars)):
+            if self.vars[i] != other.vars[i]:
+                return False
+        return True
+
+    def __sub__(self, other: Self) -> Self:
+        vars = deepcopy(self.vars)
+        for i in range(len(self.vars)):
+            vars[i].value = self.vars[i].value - other.vars[i].value
+        return StateDiff(vars)
+
+
+class StateDiff(State):
+
+    def __init__(self, vars: list[StateVar]):
+        super().__init__(vars)
+        for var in self.vars:
+            if var.value != 0 and var.reversible:
+                var.value = inf
+
+    def __str__(self) -> str:
+        mask = {inf: "*", 0: "."}
+        return "".join(
+            [mask[x.value] if x.value in mask else str(x.value) for x in self.vars]
+        )
+
+    def relevant(self) -> StateVar:
+        return next(filter(lambda x: x.value != 0, self.vars))
+
+    def __abs__(self) -> int:
+        return reduce(
+            lambda acc, x: acc + abs(1 if x.value == inf else x.value), self.vars, 0
+        )
+
+
+class Enclave:
+    mechanism: StateDiff | None
+    turned_on: list[State] = []
+
+    def __init__(self, mechanism: StateDiff | None):
+        self.mechanism = mechanism
+
+    def __str__(self) -> str:
+        return "Boss" if self.mechanism is None else str(self.mechanism)
+
+    def __eq__(self, other: Self) -> bool:
+        return self.mechanism == other.mechanism
+
+    def __hash__(self) -> int:
+        return 0 if self.mechanism is None else hash(self.mechanism)
+
+    def is_boss_enclave(self) -> bool:
+        return self.mechanism is None
+
+    def add_state(self, s: State):
+        self.turned_on.append(s)
+
+
+StateGraph = Graph[State, None]
+Dungeon = Graph[Enclave, None]
+
+
+def generate_state_graph(initial_state: State) -> StateGraph:
+    g: StateGraph = Graph()
     curr = initial_state
     g.add_node(curr)
     while not curr.all_are_max():
@@ -135,8 +188,14 @@ def generate_state_graph(initial_state: State) -> Graph[State, None]:
     return g
 
 
-def get_enclaves(state_walk):
-    pass
+def get_enclaves(state_walk) -> Dungeon:
+    g: Dungeon = Graph()
+    for i in range(1, len(state_walk)):
+        diff = state_walk[i] - state_walk[i - 1]
+        if Enclave(diff) not in g.nodes:
+            g.add_node(Enclave(diff))
+    g.add_node(Enclave(None))
+    return g
 
 
 def main(initial_state: State):
@@ -148,7 +207,11 @@ def main(initial_state: State):
     state_walk = state_graph.random_walk()
     print("STATE WALK")
     print(", ".join(map(lambda x: str(x), state_walk)))
-    # enclaves = get_enclaves(state_walk)
+    print("")
+
+    enclaves = get_enclaves(state_walk)
+    print("ENCLAVES")
+    print(enclaves)
 
 
 if __name__ == "__main__":
