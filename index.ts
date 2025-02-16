473dbfc4edc795aa029915f48bef4fc14ad19343
Alex Lugo <alugocp@aim.com>
2025-02-16 13:10:34 -0500

    Random walk method

--- index.ts
+++ index.ts
@@ -23,58 +23,45 @@ class State {
         }
     }
 
-    toString(): string {
-        return this.vars.reduce((acc, v) => `${acc}${v.id}${v.value}`, "");
-    }
+    toString = (): string =>
+        this.vars.reduce((acc, v) => `${acc}${v.id}${v.value}`, "");
 
-    relevant(): State {
-        return new State(
-            ...this.vars.filter((x) => x.reversible || x.value > 0),
-        );
-    }
+    relevant = (): State =>
+        new State(...this.vars.filter((x) => x.reversible || x.value > 0));
 
-    satisfies(s: State): boolean {
-        return s.vars.reduce(
+    satisfies = (s: State): boolean =>
+        s.vars.reduce(
             (acc, x) =>
                 acc && this.vars.find((y) => y.id === x.id)?.value === x.value,
             true,
         );
-    }
 
-    equals(s: State): boolean {
-        return (
-            this.vars.length === s.vars.length &&
-            this.vars.reduce(
-                (acc: boolean, x: StateVar, i: number) =>
-                    acc && x.id === s.vars[i].id && x.value === s.vars[i].value,
-                true,
-            )
+    equals = (s: State): boolean =>
+        this.vars.length === s.vars.length &&
+        this.vars.reduce(
+            (acc: boolean, x: StateVar, i: number) =>
+                acc && x.id === s.vars[i].id && x.value === s.vars[i].value,
+            true,
         );
-    }
 }
 
 class Enclave {
     constructor(public readonly mechanism: StateVar) {}
 
-    toString(): string {
-        return `${this.mechanism.id}(${this.mechanism.reversible ? "r" : "i"}${this.mechanism.states})`;
-    }
+    toString = (): string =>
+        `${this.mechanism.id}(${this.mechanism.reversible ? `r${this.mechanism.states}` : "b"})`;
 
-    changed(): State {
-        return new State({
-            ...this.mechanism,
-            value: this.mechanism.reversible
-                ? Math.floor(Math.random() * this.mechanism.states)
-                : Math.floor(Math.random() * (this.mechanism.states - 1)) + 1,
-        });
-    }
+    mutable = (): boolean =>
+        this.mechanism.reversible || this.mechanism.value === 0;
 
-    equals(s: Enclave): boolean {
-        return (
-            this.mechanism.id === s.mechanism.id &&
-            this.mechanism.value === s.mechanism.value
-        );
-    }
+    equals = (s: Enclave): boolean => this.mechanism.id === s.mechanism.id;
+
+    activated = (): StateVar => ({
+        ...this.mechanism,
+        value: this.mechanism.reversible
+            ? Math.floor(Math.random() * this.mechanism.states)
+            : Math.floor(Math.random() * (this.mechanism.states - 1)) + 1,
+    });
 }
 
 type EnclaveAndState = {
@@ -114,26 +101,12 @@ class Graph {
         const visited: EnclaveAndState[] = [start];
         while (adjacent.length > 0) {
             const root: EnclaveAndState = adjacent.pop()!;
-            const alternates: EnclaveAndState[] = [];
-            if (root.enclave.mechanism.reversible) {
-                for (let a = 0; a < root.enclave.mechanism.states; a++) {
-                    alternates.push({
-                        enclave: root.enclave,
-                        state: new State(...root.state.vars, {
-                            ...root.enclave.mechanism,
-                            value: a,
-                        }),
-                    });
-                }
-            } else {
-                alternates.push({
-                    enclave: root.enclave,
-                    state: new State(...root.state.vars, {
-                        ...root.enclave.mechanism,
-                        value: 1,
-                    }),
-                });
-            }
+            const alternates: EnclaveAndState[] = getAlternates(
+                root.enclave.mechanism,
+            ).map((x) => ({
+                enclave: root.enclave,
+                state: new State(...root.state.vars, { ...x, value: x.value }),
+            }));
             for (const current of alternates) {
                 const neighbors = this.edges.filter(
                     (x) =>
@@ -164,41 +137,78 @@ class Graph {
         return visited;
     }
 
-    getStateFromPath(
+    getStatesFromPath = (
         initialState: State,
         src: Enclave,
         dst: Enclave,
-    ): State | null {
-        const accessible = this.getAccessibleEnclaves({
+    ): State[] =>
+        this.getAccessibleEnclaves({
             state: initialState,
             enclave: src,
-        });
-        const valid = accessible.filter((x) => x.enclave.equals(dst));
-        return valid.length > 0 ? choice(valid).state : null;
-    }
-
-    getStateFromLoop(
-        initialState: State,
-        src: Enclave,
-        dst: Enclave,
-        diff: State | null,
-    ): State | null {
-        const state = this.getStateFromPath(initialState, src, dst);
-        return state
-            ? this.getStateFromPath(
-                  new State(...state.vars, ...(diff ? diff.vars : [])),
-                  dst,
-                  src,
-              )
-            : null;
-    }
+        })
+            .filter((x) => x.enclave.equals(dst))
+            .map((x) => x.state);
 }
 
 const choice = <E>(a: E[]): E => a[Math.floor(Math.random() * a.length)];
 
+function getAlternates(sv: StateVar): StateVar[] {
+    if (sv.reversible) {
+        const alternates: StateVar[] = [];
+        for (let a = 0; a < sv.states; a++) {
+            alternates.push({ ...sv, value: a });
+        }
+        return alternates;
+    }
+    return sv.value === 0 ? [sv, { ...sv, value: 1 }] : [sv];
+}
+
+function buildDungeon(initialState: State): Graph {
+    const graph = new Graph();
+    let currentState: State = initialState;
+    graph.nodes.push(new Enclave(initialState.vars[0]));
+    let currentEnclave = graph.nodes[0];
+    for (let a = 1; a < initialState.vars.length; a++) {
+        const anchor: Enclave = choice(graph.nodes);
+        const mutables = graph.nodes.filter((x) => x.mutable());
+        const numChanges =
+            Math.floor(Math.random() * (mutables.length - 1)) + 1;
+        const enclave: Enclave = new Enclave(initialState.vars[a]);
+
+        let conditions: StateVar[] = [];
+        for (let b = 0; b < numChanges; b++) {
+            const diff = choice(mutables);
+            mutables.splice(mutables.indexOf(diff), 1);
+            const states = graph.getStatesFromPath(
+                currentState,
+                currentEnclave,
+                diff,
+            );
+            if (states.length > 0) {
+                currentState = choice(states);
+            } else {
+                graph.addEdge(currentEnclave, diff, currentState.relevant());
+            }
+            const activated = diff.activated();
+            currentEnclave = diff;
+            conditions.push(activated);
+            currentState = new State(...currentState.vars, activated);
+        }
+        if (
+            graph.getStatesFromPath(currentState, currentEnclave, anchor)
+                .length === 0
+        ) {
+            graph.addEdge(currentEnclave, anchor, currentState.relevant());
+        }
+        graph.nodes.push(enclave);
+        graph.addEdge(anchor, enclave, new State(...conditions));
+    }
+    return graph;
+}
+
 if (
     process.argv.length != 3 ||
-    !process.argv[2].match(/[ri][0-9]+(:[ri][0-9]+)*/)
+    !process.argv[2].match(/^(b|(r[0-9]+))(:(b|(r[0-9]+)))*$/)
 ) {
     console.log(
         [
@@ -216,57 +226,14 @@ if (
     process.exit(1);
 }
 
-const initialState: State = new State(
-    ...process.argv[2].split(":").map((x: string, i: number) => ({
-        reversible: x[0] === "r",
-        id: String.fromCharCode(65 + i),
-        states: parseInt(x.substring(1)),
-        value: 0,
-    })),
+const dungeon = buildDungeon(
+    new State(
+        ...process.argv[2].split(":").map((x: string, i: number) => ({
+            reversible: x[0] === "r",
+            id: String.fromCharCode(65 + i),
+            states: x[0] === "b" ? 2 : parseInt(x.substring(1)),
+            value: 0,
+        })),
+    ),
 );
-
-const graph = new Graph();
-let currentState: State = initialState;
-graph.nodes.push(new Enclave(initialState.vars[0]));
-let currentEnclave = graph.nodes[0];
-for (let a = 1; a < initialState.vars.length; a++) {
-    const anchor: Enclave = choice(graph.nodes);
-    if (currentEnclave !== anchor) {
-        const path = graph.getStateFromPath(
-            currentState,
-            currentEnclave,
-            anchor,
-        );
-        if (path) {
-            currentState = path;
-        } else {
-            graph.addEdge(currentEnclave, anchor, currentState.relevant());
-        }
-    }
-
-    const enclave: Enclave = new Enclave(initialState.vars[a]);
-    const doorway: EnclaveAndState = choice(
-        graph.getAccessibleEnclaves({
-            state: currentState,
-            enclave: anchor,
-        }),
-    );
-    const condition = doorway.enclave.changed();
-    const loop = graph.getStateFromLoop(
-        currentState,
-        anchor,
-        doorway.enclave,
-        condition,
-    );
-    graph.nodes.push(enclave);
-    graph.addEdge(anchor, enclave, condition);
-    if (loop?.satisfies(condition)) {
-        currentState = loop;
-    } else if (anchor.mechanism.id !== condition.vars[0].id) {
-        const diff = anchor.changed();
-        graph.addEdge(anchor, new Enclave(condition.vars[0]), diff);
-        currentState = new State(...currentState.vars, ...diff.vars);
-    }
-    currentEnclave = enclave;
-}
-console.log(graph.toString());
+console.log(dungeon.toString());
